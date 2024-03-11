9a9d7e4d220b09195235b6940df3564a568b3c75
Alex Sanchez <lockedasanchez@gmail.com>
2024-03-11 14:41:43 -0400

    complete lab(3/11) git push

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+// Write your solution in this f
+const employee = {
+    name: "Chopper",
+    address: "36 Wilson st."
+}
+function  updateEmployeeWithKeyAndValue(employee, key, value){
+    const updatedEmployee = {...employee}
+    updatedEmployee[key]= value;
+    return updatedEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const employeeClone = {...employee};
+    delete employeeClone[key];
+    return employeeClone;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee
+}
